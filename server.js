2c4a8baf31ea58e3263d4ef821a4a00d3fbd74f0
dusdn <aa022044@gmail.com>
2023-12-19 00:41:35 +0900

    .

--- server.js
+++ server.js
@@ -17,26 +17,16 @@ app.use(bodyParser.json());
 
 var MySQLStore = require('express-mysql-session')(session);
 var sessionStore = new MySQLStore(sessionOption);
-app.use(session({  
-	key: 'session_cookie_name',
+app.use(session({
+    key: 'session_cookie_name',
     secret: '~',
-	store: sessionStore,
-	resave: false,
-	saveUninitialized: false
+    store: sessionStore,
+    resave: false,
+    saveUninitialized: false
 }))
 
-app.get('/', (req, res) => {    
+app.get('/', (req, res) => {
     req.sendFile(path.join(__dirname, '/build/index.html'));
-})
-
-app.get('/authcheck', (req, res) => {      
-    const sendData = { isLogin: "" };
-    if (req.session.is_logined) {
-        sendData.isLogin = "True"
-    } else {
-        sendData.isLogin = "False"
-    }
-    res.send(sendData);
 })
 
 app.get('/logout', function (req, res) {
@@ -53,11 +43,12 @@ app.post("/login", (req, res) => { // 데이터 받아서 결과 전송
     if (id && password) {             // id와 pw가 입력되었는지 확인
         db.query('SELECT * FROM firstmysql WHERE id = ?', [id], function (error, results, fields) {
             if (error) throw error;
-            if (results.length > 0) {       // db에서의 반환값이 있다 = 일치하는 아이디가 있다.      
 
+            if (results.length > 0) {       // db에서의 반환값이 있다 = 일치하는 아이디가 있다.      
                 if (password === results[0].password) {                  // 비밀번호가 일치하면
                     req.session.is_logined = true;      // 세션 정보 갱신
                     req.session.nickname = id;
+                    req.session.name = results[0].name;
                     req.session.save(function () {
                         sendData.isLogin = "True"
                         res.send(sendData);
@@ -65,10 +56,10 @@ app.post("/login", (req, res) => { // 데이터 받아서 결과 전송
                     db.query(`INSERT INTO logTable (created, id, action, command, actiondetail) VALUES (NOW(), ?, 'login' , ?, ?)`
                         , [req.session.nickname, '-', `React 로그인 테스트`], function (error, result) { });
                 }
-                else{                                   // 비밀번호가 다른 경우
+                else {                                   // 비밀번호가 다른 경우
                     sendData.isLogin = "로그인 정보가 일치하지 않습니다."
                     res.send(sendData);
-                }                     
+                }
             } else {    // db에 해당 아이디가 없는 경우
                 sendData.isLogin = "아이디 정보가 일치하지 않습니다."
                 res.send(sendData);
@@ -81,19 +72,20 @@ app.post("/login", (req, res) => { // 데이터 받아서 결과 전송
 });
 
 app.post("/signin", (req, res) => {  // 데이터 받아서 결과 전송
+    const name = req.body.userName;
     const id = req.body.userId;
     const password = req.body.userPassword;
     const password2 = req.body.userPassword2;
-    
+
     const sendData = { isSuccess: "" };
 
-    if (id && password && password2) {
-        db.query('SELECT * FROM firstmysql WHERE id = ?', [id], function(error, results, fields) { // DB에 같은 이름의 회원아이디가 있는지 확인
+    if (name && id && password && password2) {
+        db.query('SELECT * FROM firstmysql WHERE id = ?', [id], function (error, results, fields) { // DB에 같은 이름의 회원아이디가 있는지 확인
             if (error) throw error;
             if (results.length <= 0 && password == password2) {         // DB에 같은 이름의 회원아이디가 없고, 비밀번호가 올바르게 입력된 경우
-                db.query('INSERT INTO firstmysql (id, password) VALUES(?,?)', [id, password], function (error, data) {
+                db.query('INSERT INTO firstmysql (id, password, name) VALUES(?, ?, ?)', [id, password, name], function (error, data) {
                     if (error) throw error;
-                    req.session.save(function () {                        
+                    req.session.save(function () {
                         sendData.isSuccess = "True"
                         res.send(sendData);
                     });
@@ -104,14 +96,138 @@ app.post("/signin", (req, res) => {  // 데이터 받아서 결과 전송
             }
             else {                                                  // DB에 같은 이름의 회원아이디가 있는 경우            
                 sendData.isSuccess = "이미 존재하는 아이디 입니다!"
-                res.send(sendData);  
-            }            
-        });        
+                res.send(sendData);
+            }
+        });
     } else {
-        sendData.isSuccess = "아이디와 비밀번호를 입력하세요!"
-        res.send(sendData);  
+        sendData.isSuccess = "이름, 아이디, 비밀번호를 입력하세요!"
+        res.send(sendData);
     }
-    
+
+});
+
+app.get('/userdata', (req, res) => {
+    const sendData = { isLogin: false, userData: {} };
+    const id = req.session.nickname;
+    const sqlQuery = `
+        SELECT firstmysql.name, firstmysql.id
+        From firstmysql
+        WHERE firstmysql.id = ?`;
+    db.query(sqlQuery, [id], function (error, results, fields) {
+        if (error) throw error;
+        else {
+            const userData = results[0] || {};
+            sendData.userData = userData;
+            sendData.isLogin = true;
+            const postsQuery = `
+                SELECT posts.content, posts.idx
+                FROM posts
+                WHERE posts.id = ?`;
+            db.query(postsQuery, [id], function (error, results, fields) {
+                if (error) throw error;
+                else {
+                    sendData.userPosts = results;
+                    res.send(sendData);
+                }
+            });
+        }
+    });
+});
+
+app.get('/boards', (req, res) => {
+    db.query('SELECT firstmysql.name, posts.content, posts.idx FROM firstmysql JOIN posts ON firstmysql.id = posts.id', function (error, results, fields) {
+        if (error) {
+            console.error('Error retrieving posts data:', error);
+            res.status(500).send('Internal Server Error');
+        } else {
+            const postsData = results.map(post => ({
+                name: post.name,
+                content: post.content,
+                idx: post.idx,
+            }));
+            res.send({ allPosts: postsData });
+        }
+    });
+});
+
+app.get('/posts', (req, res) => {
+    db.query('SELECT * FROM posts', function (error, results, fields) {
+        if (error) {
+            console.error('Error retrieving posts data:', error);
+            res.status(500).send('Internal Server Error');
+        } else {
+            const postsData = results.map(post => ({
+                id: post.id,
+                content: post.content,
+                comments: []
+            }));
+        }
+    });
+});
+
+app.post('/posts', (req, res) => {
+    const id = req.session.nickname;
+    const content = req.body.userContent;
+
+    db.query('INSERT INTO posts (id, content) VALUES (?, ?)', [id, content], function (error, results, fields) {
+        if (error) {
+            console.error('Error inserting post data:', error);
+            res.status(500).send('Internal Server Error');
+        } else {
+            res.send({ isSuccess: true, postId: results.insertId });
+        }
+    });
+});
+
+app.delete('/posts', (req, res) => {
+    const id = req.session.nickname;
+
+    db.query('DELETE FROM posts WHERE id = ?', [id], function (error, results, fields) {
+        if (error) {
+            console.error('Error deleting posts data:', error);
+            res.status(500).send('Internal Server Error');
+        } else {
+            res.send({ isSuccess: true });
+        }
+    });
+});
+
+app.get('/comments/:postId', (req, res) => {
+    const postId = req.params.postId;
+    const commentsQuery = `
+        SELECT posts.content, comments.writer, comments.comment
+        FROM posts LEFT JOIN comments ON posts.idx = comments.postId
+        WHERE posts.idx = ?`;
+    db.query(commentsQuery, [postId], (error, results, fields) => {
+        if (error) {
+            console.error('Error retrieving comments data:', error);
+            res.status(500).send('Internal Server Error');
+        }
+        else {
+            const postContent = results.length > 0 ? results[0].content : '';
+            const commentsData = results.map((comment) => ({
+                writer: comment.writer,
+                comment: comment.comment,
+            }));
+            res.send({ postContent, comment: commentsData });
+        }
+    }
+    );
+});
+
+app.post('/comments/:postId', (req, res) => {
+    const writer = req.session.name;
+    const comment = req.body.comment;
+    const postId = req.params.postId;
+
+    db.query('INSERT INTO comments (writer, comment, postId) VALUES (?, ?, ?)', [writer, comment, postId], function (error, result) {
+        if (error) {
+            console.error('Error inserting comment data:', error);
+            res.status(500).send('Internal Server Error');
+        } else {
+            res.json({ isSuccess: true, writer: writer });
+        }
+    });
 });
 
 app.listen(port, () => {
--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import { useState, useEffect } from 'react';
-import { useNavigate, BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { useNavigate, BrowserRouter, Routes, Route, Link, useParams } from 'react-router-dom';
 import axios from 'axios';
 
 function Main() {
   return (
     <>
-      <h2>마이페이지</h2>
+      <h2>홈페이지</h2>
       <Link to="/login">로그인</Link>
+      <span className="link-gap">|</span>
+      <Link to="/signin">회원가입</Link>
     </>
   );
 }
@@ -16,7 +18,7 @@ function Login(props) {
   const navigate = useNavigate();
   const [id, setId] = useState("");
   const [password, setPassword] = useState("");
-  
+
   return <>
     <h2>로그인</h2>
 
@@ -35,9 +37,8 @@ function Login(props) {
         };
         axios.post("http://localhost:3001/login", userData)
           .then((response) => {
-            console.log("data: ", response.data);
-            const json = response.data;       
-            if(json.isLogin==="True"){
+            const json = response.data;
+            if (json.isLogin === "True") {
               navigate('/welcome');
             }
             else {
@@ -53,12 +54,13 @@ function Login(props) {
     <p>계정이 없으신가요?  <button onClick={() => {
       navigate('/signin');
     }}>회원가입</button></p>
-  </> 
+  </>
 }
 
 
 function Signin(props) {
   const navigate = useNavigate();
+  const [name, setName] = useState("");
   const [id, setId] = useState("");
   const [password, setPassword] = useState("");
   const [password2, setPassword2] = useState("");
@@ -67,6 +69,9 @@ function Signin(props) {
     <h2>회원가입</h2>
 
     <div className="form">
+      <p><input className="login" type="text" placeholder="이름" onChange={event => {
+        setName(event.target.value);
+      }} /></p>
       <p><input className="login" type="text" placeholder="아이디" onChange={event => {
         setId(event.target.value);
       }} /></p>
@@ -79,6 +84,7 @@ function Signin(props) {
 
       <p><input className="btn" type="submit" value="회원가입" onClick={() => {
         const userData = {
+          userName: name,
           userId: id,
           userPassword: password,
           userPassword2: password2,
@@ -86,11 +92,11 @@ function Signin(props) {
         axios.post("http://localhost:3001/signin", userData)
           .then((response) => {
             const json = response.data;
-            if(json.isSuccess==="True"){
+            if (json.isSuccess === "True") {
               alert('회원가입이 완료되었습니다!')
               navigate('/login');
             }
-            else{
+            else {
               alert(json.isSuccess)
             }
           })
@@ -103,35 +109,218 @@ function Signin(props) {
     <p>로그인화면으로 돌아가기  <button onClick={() => {
       navigate('/login');
     }}>로그인</button></p>
-  </> 
+  </>
 }
 
-function Welcome() {
+function Welcome(props) {
+  const navigate = useNavigate();
+
+  const handleBoards = () => {
+    navigate('/boards');
+  };
+  const handleLogout = () => {
+    navigate('/')
+  }
   return (
     <>
-      <h2>메인 페이지에 오신 것을 환영합니다</h2>
-      <p>로그인에 성공하셨습니다.</p>
-      <Link to="/">로그인</Link>
+      <h3>Home</h3>
+      <Mypage />
+      <button className="btn" onClick={handleBoards}>게시글 조회</button>
+      <button className="btn" onClick={handleLogout}>로그아웃</button>
     </>
   );
 }
 
-function App() {
+function Mypage() {
+  const navigate = useNavigate();
+  const [userData, setUserData] = useState({});
+  const [userPosts, setUserPosts] = useState([]);
+
+  const handleComments = (postId) => {
+    navigate(`/comments/${postId}`);
+  };
+
   useEffect(() => {
-    axios.post('http://localhost:3001/authcheck')
+    axios.get("http://localhost:3001/userdata", userData)
       .then(response => {
-        const json = response.data;  
-        if (json.isLogin === "True") {
-          window.location.replace('/welcome');
-        }
-        else {
-          window.location.replace('/login');
-        }
+        const json = response.data;
+        setUserData(json.userData);
+        setUserPosts(json.userPosts);
       })
       .catch(error => {
         console.error('Error checking authentication:', error);
       });
-  }, []); 
+  }, []);
+
+  const handleDeletePost = () => {
+    axios.delete("http://localhost:3001/posts")
+      .then(response => {
+        const json = response.data;
+        if (json.isSuccess) {
+          setUserPosts([]);
+        } else {
+          console.log('게시글 삭제에 실패했습니다.');
+        }
+      })
+      .catch(error => {
+        console.error('Error deleting post:', error);
+      });
+  };
+
+  return (
+    <>
+      <h1>마이페이지</h1>
+      <>
+        <li>
+          <p><strong>이름:</strong> {userData.name}</p>
+          <p><strong>아이디:</strong> {userData.id}</p>
+        </li>
+        <button className="btn" onClick={handleDeletePost}>모든 게시글 삭제</button>
+        <p><strong>내가 작성한 게시글:</strong></p>
+        <ul>
+          {userPosts.map((post, index) => (
+            <li key={index} onClick={() => handleComments(post.idx)}>
+              <strong>내용:</strong>{post.content}
+            </li>
+          ))}
+        </ul>
+      </>
+    </>
+  );
+}
+
+function Board() {
+  const navigate = useNavigate();
+  const [allPosts, setAllPosts] = useState([]);
+
+  const handlePosts = () => {
+    navigate('/posts');
+  }
+  const handleMypage = () => {
+    navigate('/welcome');
+  }
+  const handleComments = (postId) => {
+    navigate(`/comments/${postId}`);
+  };
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/boards")
+      .then(response => {
+        const json = response.data;
+        setAllPosts(json.allPosts);
+      })
+      .catch(error => {
+        console.error('Error fetching posts:', error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h2>모든 게시글 조회</h2>
+      <button className="btn" onClick={handlePosts}>글쓰기</button>
+      <ul>
+        {allPosts.map((post, index) => (
+          <li key={index} onClick={() => handleComments(post.idx)}>
+            <strong>작성자:</strong> {post.name} <strong>내용:</strong> {post.content}
+          </li>
+        ))}
+      </ul>
+      <button className="btn" onClick={handleMypage}>돌아가기</button>
+    </div>
+  );
+}
+
+function Post() {
+  const navigate = useNavigate();
+  const [content, setContent] = useState('');
+
+  const handleMypage = () => {
+    navigate('/boards');
+  }
+
+  const handleSubmit = () => {
+    const postData = {
+      userContent: content,
+    };
+
+    axios.post("http://localhost:3001/posts", postData)
+      .then(response => {
+        const json = response.data;
+        if (json.isSuccess) {
+          alert('게시글이 작성되었습니다!');
+        }
+        else {
+          alert('게시글 작성에 실패했습니다.');
+        }
+      })
+      .catch(error => {
+        console.error("Axios POST request error: ", error);
+      });
+  };
+
+  return (
+    <div>
+      <h2>게시글 작성</h2>
+      <textarea className="login" value={content} onChange={(e) => setContent(e.target.value)}></textarea>
+      <button className="btn" onClick={handleSubmit}>업로드</button>
+      <button className="btn" onClick={handleMypage}>목록</button>
+    </div>
+  );
+}
+
+function Comment() {
+  const { postId } = useParams();
+  const [postContent, setPostContent] = useState('');
+  const [comments, setComments] = useState([]);
+  const [newComment, setNewComment] = useState('');
+
+  useEffect(() => {
+    axios.get(`http://localhost:3001/comments/${postId}`)
+      .then(response => {
+        const json = response.data;
+        setPostContent(json.postContent || '');
+        setComments(json.comment || []);
+      })
+      .catch(error => {
+        console.error('Error fetching comments:', error);
+      });
+  }, [postId]);
+
+  const handleSubmitComment = () => {
+    axios.post(`http://localhost:3001/comments/${postId}`, { comment: newComment, postId: postId })
+      .then(response => {
+        const json = response.data;
+        if (json.isSuccess) {
+          setComments(prevComments => [...prevComments, { writer: json.writer, comment: newComment }]);
+          setNewComment('');
+        } else {
+          alert('댓글 작성에 실패했습니다.');
+        }
+      })
+      .catch(error => {
+        console.error('Error posting comment:', error);
+      });
+  };
+
+  return (
+    <div className="comment-container">
+      <Link to="/boards">게시글 목록</Link>
+      <p className="post-content" style={{ fontSize: '25px' }}>{postContent}</p>
+      <h2 style={{ fontSize: '1rem' }} className="comment-heading">댓글</h2>
+      <ul className="comment-list">
+        {comments.map((comment, index) => (
+          <li key={index} className="comment-item">{comment.writer}: {comment.comment}</li>
+        ))}
+      </ul>
+      <div className="comment-input-container">
+        <textarea value={newComment} onChange={(e) => setNewComment(e.target.value)} className="comment-input"></textarea>
+        <button className="comment-btn" onClick={handleSubmitComment}>댓글 작성</button>
+      </div>
+    </div>
+  );
+}
+
+function App() {
 
   return (
     <BrowserRouter>
@@ -141,6 +330,9 @@ function App() {
           <Route path='/login' element={<Login />}></Route>
           <Route path='/signin' element={<Signin />}></Route>
           <Route path='/welcome' element={<Welcome />}></Route>
+          <Route path='/boards' element={<Board />}></Route>
+          <Route path='/posts' element={<Post />}></Route>
+          <Route path='/comments/:postId' element={<Comment />}></Route>
         </Routes>
       </div>
     </BrowserRouter>
